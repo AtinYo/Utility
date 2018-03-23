0f07bf33e7c6a476639330f9da28057861b0953e
AtinYo <18202408637@163.com>
2018-03-23 11:45:57 +0800

    modify something
    Modify it so that we can use it in Unity 5.x

--- Utility/Utility/Interfaces/Interfaces.cs
+++ Utility/Utility/Interfaces/Interfaces.cs
@@ -19,18 +19,17 @@ namespace Utility.Interfaces
                     lock (mutex)
                     {
                         var ctors = typeof(T).GetConstructors(System.Reflection.BindingFlags.Instance |
-                            System.Reflection.BindingFlags.NonPublic |
-                            System.Reflection.BindingFlags.Public);//指定搜索公有constructor是为了报异常
-                        if (ctors.Count() != 1)
+                                System.Reflection.BindingFlags.NonPublic |
+                                System.Reflection.BindingFlags.Public);//指定搜索公有constructor是为了报异常
+                        if (ctors.Length != 1)
                         {
                             throw new InvalidOperationException(String.Format("Type {0} must have exactly one constructor.", typeof(T)));
                         }
-                        var ctor = ctors.SingleOrDefault(c => c.GetParameters().Count() == 0 && c.IsPrivate);//构造函数必须是无参且私有的
-                        if(ctor == null)
+                        if (ctors[0].GetParameters().Length != 0 || !ctors[0].IsPrivate)//构造函数必须是无参且私有的
                         {
                             throw new InvalidOperationException(String.Format("The constructor for {0} must be private and take no parameters.", typeof(T)));
                         }
-                        m_instance = ctor.Invoke(null) as T;
+                        m_instance = ctors[0].Invoke(null) as T;
                     }
                 }
                 return m_instance;
