4e310d5d21f077b6cff0d2de7a5bac59cccb743b
AtinYo <18202408637@163.com>
2018-03-23 11:37:53 +0800

    Modify the singleton implement
    A common implement.

--- Utility/Utility/Interfaces/Interfaces.cs
+++ Utility/Utility/Interfaces/Interfaces.cs
@@ -6,18 +6,34 @@ using System.Threading.Tasks;
 
 namespace Utility.Interfaces
 {
-    public class TSingleton<T>where T : class, new()
+    public class TSingleton<T>where T : class
     {
-        protected static T m_Instance;
+        private static object mutex = new Object();
+        protected static volatile T m_instance = null;//volatile允许任意线程修改
         public static T Instance
         {
             get
             {
-                if (m_Instance == null)
+                if (m_instance == null)
                 {
-                    m_Instance = new T();
+                    lock (mutex)
+                    {
+                        var ctors = typeof(T).GetConstructors(System.Reflection.BindingFlags.Instance |
+                            System.Reflection.BindingFlags.NonPublic |
+                            System.Reflection.BindingFlags.Public);//指定搜索公有constructor是为了报异常
+                        if (ctors.Count() != 1)
+                        {
+                            throw new InvalidOperationException(String.Format("Type {0} must have exactly one constructor.", typeof(T)));
+                        }
+                        var ctor = ctors.SingleOrDefault(c => c.GetParameters().Count() == 0 && c.IsPrivate);//构造函数必须是无参且私有的
+                        if(ctor == null)
+                        {
+                            throw new InvalidOperationException(String.Format("The constructor for {0} must be private and take no parameters.", typeof(T)));
+                        }
+                        m_instance = ctor.Invoke(null) as T;
+                    }
                 }
-                return m_Instance;
+                return m_instance;
             }
         }
     }
